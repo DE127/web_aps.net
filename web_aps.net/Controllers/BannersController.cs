221c96c1b52e9cb49b3989ca36e59da8b33824c7
Hung Nguyen <nguyenhungg127@gmail.com>
2024-04-05 14:03:30 +0700

    Add new crud

---
+++ web_aps.net/Controllers/BannersController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using web_aps.net.Models;
+
+namespace web_aps.net.Controllers
+{
+    public class BannersController : Controller
+    {
+        private readonly BDKSo3Context _context;
+
+        public BannersController(BDKSo3Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Banners
+        public async Task<IActionResult> Index()
+        {
+              return _context.Banners != null ? 
+                          View(await _context.Banners.ToListAsync()) :
+                          Problem("Entity set 'BDKSo3Context.Banners'  is null.");
+        }
+
+        // GET: Banners/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Banners == null)
+            {
+                return NotFound();
+            }
+
+            var banner = await _context.Banners
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (banner == null)
+            {
+                return NotFound();
+            }
+
+            return View(banner);
+        }
+
+        // GET: Banners/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Banners/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Status,Prioty,Image,Description")] Banner banner)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(banner);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(banner);
+        }
+
+        // GET: Banners/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Banners == null)
+            {
+                return NotFound();
+            }
+
+            var banner = await _context.Banners.FindAsync(id);
+            if (banner == null)
+            {
+                return NotFound();
+            }
+            return View(banner);
+        }
+
+        // POST: Banners/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Status,Prioty,Image,Description")] Banner banner)
+        {
+            if (id != banner.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(banner);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BannerExists(banner.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(banner);
+        }
+
+        // GET: Banners/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Banners == null)
+            {
+                return NotFound();
+            }
+
+            var banner = await _context.Banners
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (banner == null)
+            {
+                return NotFound();
+            }
+
+            return View(banner);
+        }
+
+        // POST: Banners/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Banners == null)
+            {
+                return Problem("Entity set 'BDKSo3Context.Banners'  is null.");
+            }
+            var banner = await _context.Banners.FindAsync(id);
+            if (banner != null)
+            {
+                _context.Banners.Remove(banner);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BannerExists(int id)
+        {
+          return (_context.Banners?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
